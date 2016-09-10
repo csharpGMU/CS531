63e6fc4db2784072e51cc1aa9837a460257fe5c3
Chris Sharp <csharp5@gmu.edu>
2016-09-10 15:04:17 -0400

    Skeleton start of my program

---
+++ hw1-sharp.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define NUMSTRINGS 10
+
+void main()
+{
+
+
+}
+
+void getStrings(char *stringArray[],int count)
+{
+
+
+}
+
+void freeStrings(char *stringArray[],int count)
+{
+
+
+}
+
+int stringOrder()
+{
+
+
+}
+
+int sortStrings(char *stringArray, int size)
+{
+
+  
+}
