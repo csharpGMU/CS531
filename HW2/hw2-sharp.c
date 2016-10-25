76cb43835b0c89a9c0ddf14d45c6ae968eef6bf9
Chris Sharp <csharp5@gmu.edu>
2016-10-24 21:51:23 -0400

    Finished up my version

--- HW2/hw2-sharp.c
+++ HW2/hw2-sharp.c
@@ -1,49 +1,73 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
+#include <unistd.h>
 #include <string.h>
 
+#define PATH_TO_SHELL "/bin/sh"
+
 int cs531_system(const char *comm);
+void cs531_system_SIGINT_handler(int sig);
 
 int main(int argc, char *argv[])
 {
-	if(argc>1)
-		cs531_system(argv[1]);
-	else
-		cs531_system("aaa");
+	char command[256];
 
-	return 0;
+	if(argc>1) //Execute the argument
+	{
+		cs531_system(argv[1]);
+	}
+	else //Enter interactive command mode
+	{
+		while(1==1)
+		{
+			printf("\nPlease enter system command (QUIT to exit): ");
+			fgets(command,255,stdin);
+
+			//Test for quit
+			if(strcmp(command,"QUIT\n")==0)
+			{
+				printf("\nGoodbye.\n");
+				exit(EXIT_SUCCESS);
+			}
+
+			//Execute the command
+			cs531_system(command);
+		}
+	}
 
 }
 
 int cs531_system(const char *comm)
 {
-	int pid;
+	pid_t pid;
+	int status;
 
 	//Register sigint handler
-	signal(SIGINT,cs531_system_SIGINT_handler(&pid));	
+	signal(SIGINT,cs531_system_SIGINT_handler);
 
 	//Fork
 	pid = fork();
 
 	if(pid==0)
 	{	//Executed if CHILD
-		printf("Child process");
-		execlp(comm); //Fix this
-		_exit(1);
+		printf("Child process %s\n",comm);
+		execl(PATH_TO_SHELL,PATH_TO_SHELL,"-c",comm,(char *)0);
+		printf("Child failed\n");
+		_exit(127); //Only executed on failure
 	}
-	else // Executed if PARENT
-	{
-		printf("Parent process");
-		wait();
-		printf("Child died");
+	else
+	{ //Executed if PARENT
+		printf("Parent process\n");
+		waitpid(pid,&status,0);
+		printf("Child died\n");
 	}
-	
+
 	//Deregister sigint handler
 	signal(SIGINT, SIG_DFL);
 }
 
-int cs531_system_SIGINT_handler(int *pid)
+void cs531_system_SIGINT_handler(int sig)
 {
-	printf("CTRL-C pressed; PID: %d\n",*pid);
+	printf("CTRL-C pressed\n");
 }
